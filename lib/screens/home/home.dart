c69ef673e73fec43a104bc53ab18393b1140db5f
2EZ4BLINK <blinkcode24@gmail.com>
2026-07-19 06:05:14 +0800

    feat: added a list view

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -10,6 +10,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List characters = ['mario', 'luigi', 'peach', 'toad', 'bowser', 'koopa'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,11 +22,21 @@ class _HomeState extends State<Home> {
       ),
       body: Container(
         padding: const EdgeInsets.all(16),
-        child: Column(
+        child: Column( 
           children: [
-            const StyledText("Character list"),
-            const StyledHeading("Character list"),
-            const StyledTitle("Character list"),
+            Expanded(
+              child: ListView.builder(
+                itemCount: characters.length,
+                itemBuilder: (_, index) {
+                  return  Container(
+                    color: Colors.grey[800],
+                    padding: const EdgeInsets.all(40),
+                    margin: const EdgeInsets.only(bottom: 40),
+                    child: Text(characters[index]),
+                  );
+                },
+              )
+            ),
 
             StyledButton(
                 onPressed: () => {},
