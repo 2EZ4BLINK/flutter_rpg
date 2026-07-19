ead6303e900ae72ebd0a84e24014b2294b53c92d
2EZ4BLINK <blinkcode24@gmail.com>
2026-07-19 15:57:19 +0800

    feat: added a text field and a textfield controller

--- lib/screens/create/create.dart
+++ lib/screens/create/create.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:flutter_rpg/theme.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Create extends StatefulWidget {
   const Create({super.key});
@@ -10,6 +11,17 @@ class Create extends StatefulWidget {
 }
 
 class _CreateState extends State<Create> {
+
+  final _nameController = TextEditingController();
+  final _sloganController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _sloganController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +34,10 @@ class _CreateState extends State<Create> {
         child: Column(
           children: [
             Center(
-              child: Icon(Icons.code, color: AppColors.primaryColor,),
+              child: Icon(
+                Icons.code,
+                  color: AppColors.primaryColor
+              ),
             ),
             const Center(
               child: StyledHeading("Welcome, new player."),
@@ -30,7 +45,32 @@ class _CreateState extends State<Create> {
             const Center(
               child: StyledText("Create a name and slogan for your character."),
             ),
-            const SizedBox(height: 30,)
+            const SizedBox(height: 30,),
+
+            // Form
+            TextField(
+              controller: _nameController,
+              style: GoogleFonts.kanit(
+                  textStyle: Theme.of(context).textTheme.bodyMedium
+              ),
+              cursorColor: AppColors.textColor,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.person_2),
+                label: StyledText("Character name")
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: _sloganController,
+              style: GoogleFonts.kanit(
+                  textStyle: Theme.of(context).textTheme.bodyMedium
+              ),
+              cursorColor: AppColors.textColor,
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.chat),
+                  label: StyledText("Character slogan")
+              ),
+            ),
           ],
         ),
       )
