58f083a4d6c8ba4545cf3c94f0f791e6b6de3563
2EZ4BLINK <blinkcode24@gmail.com>
2026-07-19 13:59:00 +0800

    feat: added a skill classs and image assets

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rpg/models/skill.dart';
 import 'package:flutter_rpg/models/stats.dart';
 import 'package:flutter_rpg/models/vocation.dart';
 
@@ -10,6 +11,7 @@ class Character with Stats{
     required this.id,
   });
 
+  final Set<Skill> skills = {};
   final Vocation vocation;
   final String name;
   final String slogan;
@@ -20,6 +22,11 @@ class Character with Stats{
 
   void toggleIsFav() {
     _isFav = !_isFav;
+  }
+
+  void updateSkill(Skill skill) {
+    skills.clear();
+    skills.add(skill);
   }
 
 }
