84c50c18d8efeebbca4bc0958e7f78201e8ec6e6
2EZ4BLINK <blinkcode24@gmail.com>
2026-07-19 14:43:14 +0800

    feat: added dummy data and chracter class

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_rpg/models/skill.dart';
 import 'package:flutter_rpg/models/stats.dart';
 import 'package:flutter_rpg/models/vocation.dart';
@@ -29,3 +28,10 @@ class Character with Stats{
     skills.add(skill);
   }
 }
+
+List<Character> characters = [
+  Character(id: '1', name: 'Klara', vocation: Vocation.wizard, slogan: 'Kapumf!'),
+  Character(id: '2', name: 'Jonny', vocation: Vocation.junkie, slogan: 'Light me up...'),
+  Character(id: '3', name: 'Crimson', vocation: Vocation.raider, slogan: 'Fire in the hole!'),
+  Character(id: '4', name: 'Shaun', vocation: Vocation.ninja, slogan: 'Alright then gang.'),
+];
