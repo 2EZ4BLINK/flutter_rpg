13543c08acfc126ec2935c320fbbd7f9b8382572
2EZ4BLINK <blinkcode24@gmail.com>
2026-07-19 21:41:03 +0800

    feat: passing data into routes

--- lib/screens/home/character_card.dart
+++ lib/screens/home/character_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rpg/models/character.dart';
+import 'package:flutter_rpg/screens/profile/profile.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:flutter_rpg/theme.dart';
 
@@ -10,31 +11,42 @@ class CharacterCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        child: Row(
-          children: [
-            Image.asset('assets/img/vocations/${character.vocation.image}',
-            width: 80
-            ),
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(
+          builder: (ctx) => Profile(character: character),
+        ));
+      },
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: Row(
+            children: [
+              Image.asset('assets/img/vocations/${character.vocation.image}',
+              width: 80
+              ),
 
-            const SizedBox(width: 20),
+              const SizedBox(width: 20),
 
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                StyledHeading(character.name),
-                StyledText(character.vocation.title)
-              ],
-            ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  StyledHeading(character.name),
+                  StyledText(character.vocation.title)
+                ],
+              ),
 
-            const Expanded(child: SizedBox()),
-            IconButton(
-                onPressed: () {},
-                icon: Icon( Icons.arrow_forward , color: AppColors.textColor),
-            )
-          ],
+              const Expanded(child: SizedBox()),
+              IconButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(
+                      builder: (ctx) => Profile(character: character),
+                    ));
+                  },
+                  icon: Icon( Icons.arrow_forward , color: AppColors.textColor),
+              )
+            ],
+          ),
         ),
       ),
     );
